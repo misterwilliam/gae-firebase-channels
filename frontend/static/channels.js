9f4333a468474199041f8a0e68279889d7fbc720
misterwilliam <mr.williamchang@gmail.com>
2016-05-02 12:45:55 -0700

    Implement channel closing on client side

--- frontend/static/channels.js
+++ frontend/static/channels.js
@@ -16,6 +16,7 @@ function Socket(token, optional_handler) {
     this.onerror = optional_handler.onerror != null ? optional_handler.onerror : function(){};
     this.onclose = optional_handler.onclose != null ? optional_handler.onclose : function(){};
   }
+
   this._connect();
 }
 
@@ -31,29 +32,59 @@ Socket.prototype._connect = function() {
       presenceRef.set("connected");
       presenceRef.onDisconnect().set("disconnected");
       // Subscribe to channel status events
-      fbRef.child("channels")
-           .child(authData.auth.channel_id)
-           .child("_meta/status")
-           .on('value',
-                function(snapshot, prevChildKey) {
-                  if (snapshot.exists()) {
-                    if (snapshot.val() == "open") {
-                      this.onopen();
-                    } else if (snapshot.val() == "closed") {
-                      this.onclose();
-                    }
-                  }
-                }.bind(this));
+      this.channelStatusRef = fbRef.child("channels")
+                                   .child(authData.auth.channel_id)
+                                   .child("_meta/status");
+      this.valueEventHandler = function(snapshot, prevChildKey) {
+        if (snapshot.exists()) {
+          if (snapshot.val() == "open") {
+            this.onopen();
+          } else if (snapshot.val() == "closed") {
+            console.log("cleaned up");
+            this.onclose();
+            this._disconnect();
+          }
+        }
+      }.bind(this);
+      this.channelStatusRef.on('value', this.valueEventHandler);
       // Subscribe to messages
-      fbRef.child("channels")
-           .child(authData.auth.channel_id)
-           .child("messages")
-           .on('child_added',
-              function(snapshot, prevChildKey) {
-                if (snapshot.exists()) {
-                  this.onmessage(snapshot.val());
-                }
-              }.bind(this));
+      this.messagesRef = fbRef.child("channels")
+                              .child(authData.auth.channel_id)
+                              .child("messages");
+      this.childEventHandler = function(snapshot, prevChildKey) {
+        if (snapshot.exists()) {
+          this.onmessage(snapshot.val());
+        }
+      }.bind(this);
+      this.messagesRef.on('child_added', this.childEventHandler);
     }
   }.bind(this));
+
+  this.authEventHandler = function(authData) {
+    if (authData == null) {
+      var didDisconnect = this._disconnect();
+      if (didDisconnect) {
+        this.onclose();
+        fbRef.offAuth(this.authEventHandler);
+      }
+    }
+  }.bind(this);
+  fbRef.onAuth(this.authEventHandler);
+}
+
+Socket.prototype._disconnect = function() {
+  var didDisconnect = false;
+  if (this.channelStatusRef != null && this.valueEventHandler != null) {
+    this.channelStatusRef.off('value', this.valueEventHandler);
+    this.channelStatusRef = null;
+    this.valueEventHandler = null;
+    didDisconnect = true;
+  }
+  if (this.messagesRef != null && this.childEventHandler != null) {
+    this.messagesRef.off('child_added', this.childEventHandler);
+    this.messagesRef = null;
+    this.childEventHandler = null;
+    didDisconnect = true;
+  }
+  return didDisconnect;
 }
